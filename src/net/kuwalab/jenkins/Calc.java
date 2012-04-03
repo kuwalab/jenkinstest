8965dfb0543f0fcb332206dc220def68d05223a3
kuwalab <arata.shinichi@gmail.com>
2012-04-03 17:46:23 +0900

    新規作成

---
+++ src/net/kuwalab/jenkins/Calc.java
@@ -0,0 +1,7 @@
+package net.kuwalab.jenkins;
+
+public class Calc {
+	public int add(int x, int y) {
+		return x + y;
+	}
+}
---
+++ test/net/kuwalab/jenkins/CalcTest.java
@@ -0,0 +1,14 @@
+package net.kuwalab.jenkins;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalcTest {
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+}
